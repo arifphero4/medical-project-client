9b3ac3d39b0e5ca9930651ea9e989cb998a00985
arifphero <ahammadarif232@gmail.com>
2022-05-19 10:56:42 +0600

    appointment component added

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import Navbar from "./Pages/Shared/Navbar";
 import Login from "./Pages/Login/Login";
-import Footer from "./Pages/Shared/Footer";
+import Appointment from "./Pages/Appointment/Appointment";
 
 function App() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto px-12">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="home" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
+        <Route path="appointment" element={<Appointment />} />
       </Routes>
-      <Footer />
     </div>
   );
 }
