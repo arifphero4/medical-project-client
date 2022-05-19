b125a4a8eef75ff8b9a050b50a94e75dd3cff0ae
arifphero <ahammadarif232@gmail.com>
2022-05-19 13:25:45 +0600

    loading data from database

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -8,13 +8,13 @@ const AvailableAppointment = ({ date }) => {
   const [treatment, setTreatment] = useState(null);
 
   useEffect(() => {
-    fetch("services.json")
+    fetch("http://localhost:5000/service")
       .then((res) => res.json())
       .then((data) => setServices(data));
   }, []);
   return (
     <div>
-      <h3 className="text-xl text-secondary text-center">
+      <h3 className="text-xl text-secondary text-center my-12">
         Available Services on {format(date, "PP")}
       </h3>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
